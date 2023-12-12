e2517d8d043dabcf708aab74ecbc787ad15f48e1
Alva <a20fuller@student.ccn.ac.uk>
2023-12-12 23:06:14 +0000

    I have dementia
    I have dementia

--- Main.py
+++ Main.py
@@ -329,7 +329,30 @@ class Main:
                     
                     
                 if self.changing_state_to == "level 5":
-                    pass
+                    
+                    
+                    self.wintile = [5,1]
+
+                    self.level_to_level = True
+
+                    self.current_presents = []
+
+                    self.current_presents.append(Golden_Present(self,(100,0,100),[1,2],1,1,"Golden_Car"))
+
+                    self.current_presents.append(Present_Hori(self,(255,0,0),[0,3],3,1,"Anuc"))
+                    self.current_presents.append(Present_Hori(self,(255,0,0),[2,0],3,1,"Anuc"))
+                    self.current_presents.append(Present_Vert(self,(255,0,0),[1,0],1,2,"Car_1x2"))
+                    self.current_presents.append(Present_Vert(self,(255,0,0),[4,1],1,2,"Car_1x2"))
+
+
+                    self.current_presents.append(Present_Rock(self,(255,0,0),[0,2],1,1,"Rock"))
+                    self.current_presents.append(Present_Rock(self,(255,0,0),[2,2],1,1,"Rock"))
+                    
+                    self.current_presents.append(Collectible(self,(255,0,0),[0,0],1,1,"Pocket_Knife"))
+
+                    
+
+                    self.active_present = self.current_presents[0]
                     
 
 
