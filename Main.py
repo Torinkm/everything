4f3733680a6118b8a70fdea57c41fe7342848ee9
Alva <a20fuller@student.ccn.ac.uk>
2023-12-13 17:10:19 +0000

    GRUH
    AAAAAAAAA

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ class Main:
         self.keys = [pg.K_a, pg.K_b, pg.K_c, pg.K_d, pg.K_e, pg.K_f, pg.K_g, pg.K_h, pg.K_i, pg.K_j, pg.K_k, pg.K_l, pg.K_m,
                     pg.K_n, pg.K_o, pg.K_p, pg.K_q, pg.K_r, pg.K_s, pg.K_t, pg.K_u, pg.K_v, pg.K_w, pg.K_x, pg.K_y, pg.K_z]
 
-        self.levels = ["level 1","level 2", "level 3","level 4","level 5","level 6"]
+        self.levels = ["level 1","level 2", "level 3","level 4","level 5","level 6","level 7"]
 
         self.all_cutscenes = [Cutscene(self,["2.1","2.2","2.3"],"level 1"),
                                 Cutscene(self,["3.1","3.2"],"level 4"),
