549fecff2d7261dd2a5c651beb6e31cf88b3c8da
Alva <a20fuller@student.ccn.ac.uk>
2023-12-14 18:56:37 +0000

    bg 5 fix

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ class Main:
         self.jukebox = os.listdir("Music")
         pg.mixer.music.load(f"Music\{rng.choice(self.jukebox)}")
         pg.mixer.music.play(1)
-        pg.mixer.music.set_volume(0.5)
+        pg.mixer.music.set_volume(0)
         self.running = True #Switch to false when game quit
         self.state = "title_screen" #game state, changes for states like paused and game over
         self.mousedown = False
